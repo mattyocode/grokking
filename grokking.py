720f077d8f723857ecbc16261af6adb2e0ffab9d
mattyocode <matthewoliver@live.co.uk>
2020-08-11 14:40:10 +0100

    Added recursive function to count items in list

--- grokking.py
+++ grokking.py
@@ -1,4 +1,5 @@
-#C&D using recursion to sum array
+#Chapter 4 - Recursion. 
+#Write out the code for the earlier sum function.
 
 def addsum(arr):
     if len(arr) > 1:
@@ -6,4 +7,15 @@ def addsum(arr):
     else:
         return 0
 
-print(addsum([1, 2, 3, 4]))
+#print(addsum([1, 2, 3, 4]))
+
+#Write a recursive function to count the number of items in a list.
+
+def count_items(arr):
+    if len(arr) > 0:
+        arr.pop()
+        return 1 + count_items(arr)
+    else:
+        return 0
+
+print(count_items([1, 2, 3, 5, 10]))
